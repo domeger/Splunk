7463cc6d048716344f65fc8cb61cc2f62181c845
Hank Brekke <hank.brekke@code42.com>
2016-04-13 13:50:03 -0500

    Added MIT License to each file.

--- utils/c42api/storage_server.py
+++ utils/c42api/storage_server.py
@@ -1,3 +1,23 @@
+# Copyright (c) 2015 - 2016 Code42 Software, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 """
 A file to house the function, and the many functions it uses, that finds all
 storage servers for either a list of plan uids or for every plan uid
@@ -191,7 +211,7 @@ def _server_object_for_destination(authority, plan_uid, destination_guid, storag
         hostname = node_url.split(":")[1][2:]
         if not _network_test(authority, hostname):
             LOG.debug("Network test determined location unreachable")
-            return None
+        return None
     except HTTPError:
         # The server doesn't seem to have this resource
         LOG.warn("Network test failed so we will assume location is reachable")
